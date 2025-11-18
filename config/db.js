ef6746e741705363e9974d3ca5cd5dc051487532
Vaibhav Singh <vaibhavsingh9301@gmail.com>
2025-11-18 15:53:52 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/vibelink';
     try {
-        // mongoose v6+ doesn't require useNewUrlParser/useUnifiedTopology; pass the URI directly
         await mongoose.connect(mongoUri);
         console.log('MongoDB connected successfully');
     } catch (error) {
